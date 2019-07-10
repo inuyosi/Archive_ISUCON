ad7be5481d5cf6ca09004b89b977646aefc903b5
inuyosi <inuyosi@0xe91b.oit.ac.jp>
2019-07-10 18:53:22 +0900

    create file

--- ictsc/study_meeting/contest-site/app.py
+++ ictsc/study_meeting/contest-site/app.py
@@ -20,13 +20,22 @@ def problem():
     open_file.close()
     return result
 
-@app.route('/problem/<problem_name>')
+@app.route('/problem/<problem_name>',methods=["GET"])
 def problem_name(problem_name):
-    open_file = open("static/" + problem_name + ".md","r")
-    content = open_file.read()
-    result = render_template('problem.html',content=md.convert(content),problem=problem_name)
-    open_file.close()
-    return result
+    if problem_name == "Infrastructure" :
+        return render_template('Infrastructure.html')
+    try:
+        open_file = open("static/" + problem_name + ".md","r")
+        content = open_file.read()
+        result = render_template('problem.html',content=md.convert(content),problem=problem_name)
+        open_file.close()
+        return result
+    except IOError:
+        open_file = open("static/404.md","r")
+        content = open_file.read()
+        result = render_template('index.html',text=md.convert(content))
+        open_file.close()
+        return result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80,threaded=True)
