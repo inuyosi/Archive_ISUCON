6a49b4b0f5d6098381f41822354f5838db1e59c2
inuyosi <inuyosi@0xe91b.oit.ac.jp>
2019-07-12 17:51:51 +0900

    edit file

--- ictsc/study_meeting/contest-site/app.py
+++ ictsc/study_meeting/contest-site/app.py
@@ -2,9 +2,14 @@ from flask import Flask,render_template,request
 import markdown
 import copy
 import re
+from pathlib import Path
+import os
 
 app = Flask(__name__)
 md = markdown.Markdown(extensions=['tables'])
+name_list =["NADYDS","open_mouth"]
+problem_flag = False
+answer_flag = False
 
 post_data = {}
 post_data["default"] = {"team-name":'null',"comment":'null',"parsentage":0}
@@ -20,26 +25,40 @@ def index():
 @app.route('/problem')
 def problem():
     problem_parsentage = '## 問題正答/解答状況\n'
-    open_file = open("static/problem.md","r")
-    content = open_file.read()
+    content ="## 問題一覧\n"
+    for name in name_list :
+        path = Path("static/problem/" + name)
+        content += "####" + name + " 解答問題\n"
+        for i in list(path.glob("*.md")) :
+            if problem_flag :
+                data = str(i).replace("static/","").replace(".md","")
+                content += ("- ["+ data.replace("problem/"+ name +"/","") + \
+                           "](http://150.89.233.27/"+ data +")\n" )
+
     for key in post_data :
         if not key == 'default' :
             problem_parsentage += "- [" + key + "](http://150.89.233.27/problem/" + \
                                 key +"/check) " + str(post_data[key]["parsentage"]) + \
                                  "% / 100%\n"
-    result = render_template('problem.html',content=md.convert(content),problem_parsentage=md.convert(problem_parsentage))
-    open_file.close()
+    result = render_template('problem.html',content=md.convert(content),\
+             problem_parsentage=md.convert(problem_parsentage))
     return result
 
-@app.route('/problem/<problem_name>',methods=["GET"])
-def problem_name(problem_name):
-#    if problem_name == "Infrastructure" :
-#        result = render_template('Infrastructure.html')
-#    else: 
+@app.route('/answer')
+def answer():
+    content = "## 解説一覧\n"
+    if answer_flag :
+        content += "b"
+    result = render_template('index.html',text=md.convert(content))
+    return result
+
+@app.route('/problem/<team_name>/<problem_name>',methods=["GET"])
+def problem_name(team_name,problem_name):
         try:
-            open_file = open("static/" + problem_name + ".md","r")
+            open_file = open("static/problem/" + team_name + "/"+ problem_name + ".md","r")
             content = open_file.read()
-            result = render_template('problem.html',content=md.convert(content),problem=problem_name)
+            result = render_template('problem.html',content=md.convert(content),\
+                    problem=problem_name)
             open_file.close()
         except IOError:
             open_file = open("static/404.md","r")
@@ -49,17 +68,15 @@ def problem_name(problem_name):
 #   return result
         return result
 
-@app.route('/problem/<problem_name>',methods=["POST"])
-def problem_post(problem_name):
-    res1 = request.form['test1']
+@app.route('/problem/<team_name>/<problem_name>',methods=["POST"])
+def problem_post(team_name,problem_name):
     res3 = request.form['test3']
     if not problem_name in post_data : 
         post_data[problem_name] = copy.deepcopy(post_data["default"])
-    post_data[problem_name]['team-name'] = res1
+    post_data[problem_name]['team-name'] = team_name
     post_data[problem_name]['comment'] = str(res3.replace('\r',''))
-    print (res3.replace('\r',''))
     try:
-        open_file = open("static/"+ problem_name + ".md","r")
+        open_file = open("static/problem/"+ team_name + "/" + problem_name + ".md","r")
         result = render_template('index.html',text="Send OK")
         open_file.close()
     except IOError:
@@ -68,19 +85,13 @@ def problem_post(problem_name):
 
 @app.route('/problem/<problem_name>/check',methods=["GET"])
 def problem_check(problem_name):
-    try:
-        open_file = open("static/" + problem_name + ".md","r")
-        open_file.close()
-        if not problem_name in post_data : 
-            post_data[problem_name] = copy.deepcopy(post_data["default"])
-        result = render_template('check.html',\
-                 team_name=str(post_data[problem_name]['team-name']),\
-                 comment=md.convert(str(post_data[problem_name]['comment'])).replace('\n','<br>'))
-    except IOError:
+    if problem_name in post_data :
+        result = render_template('check.html',team_name=str(post_data[problem_name]['team-name']),comment=md.convert(str(post_data[problem_name]['comment'])).replace('\n','<br>'))
+    else :
         open_file = open("static/404.md","r")
-        content = open_file.read()
-        result = render_template('index.html',text=md.convert(content))
+        result = render_template('index.html',text=md.convert(open_file.read()))
         open_file.close()
+    print(result)
     return result
 
 @app.route('/problem/<problem_name>/check',methods=["POST"])
@@ -88,12 +99,24 @@ def parsentage_post(problem_name):
     res1 = request.form['test1']
     post_data[problem_name]['parsentage'] = res1
     try:
-        open_file = open("static/"+ problem_name + ".md","r")
+        open_file = open("static/problem/"+ problem_name + ".md","r")
         result = render_template('index.html',text="Send OK")
         open_file.close()
     except IOError:
         result = render_template('index.html',text="NOT file")
     return result
+
+@app.route('/change',methods=["GET"])
+def change_flag():
+    global answer_flag
+    global problem_flag
+    if not problem_flag :
+        problem_flag = True
+    elif answer_flag :
+        answer_flag = problem_flag = False
+    else :
+        answer_flag = True
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80,threaded=True)
