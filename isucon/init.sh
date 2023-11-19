cd22ac6ce3f3d48954e48fdac5050a3b736c5387
inuyosi <inuyosi.329@gmail.com>
2023-11-19 12:24:23 +0900

    Update init.sh

--- isucon/init.sh
+++ isucon/init.sh
@@ -18,5 +18,10 @@ sudo apt-get update
 sudo apt-get install k6　-y
 
 ## pt-query-digest
-sudo apt update
-sudo apt install percona-toolkit -y
+#sudo apt update
+#sudo apt install percona-toolkit -y
+
+## query-digester
+git clone https://github.com/kazeburo/query-digester.git
+cd query-digester/
+sudo install query-digester /usr/local/bin
