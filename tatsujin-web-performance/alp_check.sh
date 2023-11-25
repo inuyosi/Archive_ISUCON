50f666b90578459143cf47c5fd3e4017dc6a79ae
inuyosi <inuyosi.329@gmail.com>
2023-11-25 13:06:05 +0000

    mv files

--- tatsujin-web-performance/alp_check.sh
+++ tatsujin-web-performance/alp_check.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-alp json --sort sum -r -m "/api/livestream/[0-9]+/statistics","/api/user/.*/icon","/api/livestream/[0-9]+/livecomment","/api/livestream/[0-9]+/reaction","/api/livestream/[0-9]+/moderate","/api/livestream/[0-9]+/report","/api/livestream/[0-9]+/ngwords","/api/livestream/[0-9]+/enter","/api/livestream/[0-9]+/exit" -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
+alp json --sort sum -r -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
+#alp json --sort sum -r -m "/posts/[0-9]+,/@\w+" -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
+#alp json --sort sum -r -m "/posts/[0-9]+,/@\w+,/image/\d+" -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
