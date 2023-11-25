6025f3ca951a2ccc535c36318513a9786c7b9905
inuyosi <inuyosi.329@gmail.com>
2023-11-25 12:52:29 +0000

    cp files

---
+++ isucon13/alp_check.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+alp json --sort sum -r -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
+#alp json --sort sum -r -m "/posts/[0-9]+,/@\w+" -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
+#alp json --sort sum -r -m "/posts/[0-9]+,/@\w+,/image/\d+" -o count,method,uri,min,avg,max,sum < /var/log/nginx/access.log
